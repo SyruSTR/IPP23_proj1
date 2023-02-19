e24b125d9ef19d05bf4f1e13570fdf08294e5e54
Vetluzhskikh Nikita <xvetlu00@merlin.fit.vutbr.cz>
2023-02-19 21:14:24 +0100

    add codeInfo

---
+++ codeInfo.php
@@ -0,0 +1,67 @@
+<?php
+
+enum ParamTypes{
+    case variable;
+    case symbol;
+    case label;
+    case type;
+}
+
+enum SymbolType{
+    case string;
+    case integer;
+    case bool;
+    case nil;
+}
+
+$language = array(
+    //prace s pamci
+    "MOVE" => array(ParamTypes::variable,ParamTypes::symbol),
+    "CREATEFRAME" => NULL,
+    "PUSHFRAME" => NULL,
+    "POPFRAME" => NULL,
+    "DEFVAR" => array(ParamTypes::variable),
+    "CALL" => array(ParamTypes::label),
+    "RETURN" => NULL,
+    //
+    "PUSHS" => array(ParamTypes::symbol),
+    "POPS" => array(ParamTypes::variable),
+
+    "ADD" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "SUB" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "MUL" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "IDIV" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "LT" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "GT" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "EQ" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "AND" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "OR" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "NOT" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "INT2CHAR" => array(ParamTypes::variable,ParamTypes::symbol),
+    "STRI2INT" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+
+    "READ" => array(ParamTypes::variable,ParamTypes::type),
+    "WRITE" => array(ParamTypes::symbol),
+
+    "CONCAT" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "STRLEN" => array(ParamTypes::variable,ParamTypes::symbol),
+    "GETCHAR" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+    "SETCHAR" => array(ParamTypes::variable,ParamTypes::symbol,ParamTypes::symbol),
+
+    "TYPE" => array(ParamTypes::variable,ParamTypes::symbol),
+
+    "LABEL" => array(ParamTypes::label),
+    "JUMP" => array(ParamTypes::label),
+    "JUMPIFEQ" =>  array(ParamTypes::label,ParamTypes::symbol,ParamTypes::symbol),
+    "JUMPIFNEQ" => array(ParamTypes::label,ParamTypes::symbol,ParamTypes::symbol),
+    "EXIT" => array(ParamTypes::symbol),
+
+    "DPRINT" => array(ParamTypes::symbol),
+    "BREAK" => NULL,
+);
+
+
+
+
+
+?>
--- parse.php
+++ parse.php
@@ -1,39 +1,45 @@
 <?php
 ini_set('display_errors','stderr');
-
-
-$header = false;
-
-$xw = xmlwriter_open_memory();
-xmlwriter_set_indent($xw,1);
-
-xmlwriter_start_document($xw,'1.0','UTF-8');
-
-// while($line = fgets(STDIN)){
-//     echo($line);
-// }
-xmlwriter_start_element($xw, 'tag1');
-
-// Атрибут 'att1' для элемента 'tag1'
-xmlwriter_start_attribute($xw, 'att1');
-xmlwriter_text($xw, 'valueofatt1');
-xmlwriter_end_attribute($xw);
-
-xmlwriter_write_comment($xw, 'this is a comment.');
-
-// Создаём дочерний элемент
-xmlwriter_start_element($xw, 'tag11');
-xmlwriter_text($xw, 'This is a sample text, ä');
-xmlwriter_end_element($xw); // tag11
-
-xmlwriter_end_element($xw); // tag1
+include 'codeInfo.php';
 
 
 
-xmlwriter_start_pi($xw, 'php');
-xmlwriter_text($xw, '$foo=2;echo $foo;');
-xmlwriter_end_pi($xw);
 
-xmlwriter_end_document($xw);
 
-echo xmlwriter_output_memory($xw);
+while($line = fgets(STDIN)){
+    echo $line;
+}
+
+// $xw = xmlwriter_open_memory();
+// xmlwriter_set_indent($xw,1);
+
+// xmlwriter_start_document($xw,'1.0','UTF-8');
+
+// // while($line = fgets(STDIN)){
+// //     echo($line);
+// // }
+// xmlwriter_start_element($xw, 'tag1');
+
+
+// xmlwriter_start_attribute($xw, 'att1');
+// xmlwriter_text($xw, 'valueofatt1');
+// xmlwriter_end_attribute($xw);
+
+// xmlwriter_write_comment($xw, 'this is a comment.');
+
+
+// xmlwriter_start_element($xw, 'tag11');
+// xmlwriter_text($xw, 'This is a sample text, ä');
+// xmlwriter_end_element($xw); // tag11
+
+// xmlwriter_end_element($xw); // tag1
+
+
+
+// xmlwriter_start_pi($xw, 'php');
+// xmlwriter_text($xw, '$foo=2;echo $foo;');
+// xmlwriter_end_pi($xw);
+
+// xmlwriter_end_document($xw);
+
+// echo xmlwriter_output_memory($xw);
