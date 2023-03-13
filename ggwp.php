93430413da5fa2bd351dc32d90ab0153596b67c1
SyruSTR <dargonoed25@mail.ru>
2023-03-13 16:10:30 +0100

    add comments

--- ggwp.php
+++ ggwp.php
@@ -1,18 +1,60 @@
 <?php
 
-$xw = xmlwriter_open_memory();
-xmlwriter_set_indent($xw,1);
-xmlwriter_start_document($xw,'1.0','UTF-8');
 
-xmlwriter_start_element($xw,"program");
-xmlwriter_start_attribute($xw,"language");
-xmlwriter_text($xw,"IPPcode23");
-xmlwriter_end_attribute($xw);
+//10
+$strT = "string@řetězec\0322s\032lomítkem\032\092\032a\010novým\035řádkem";
+$strF = "string@řetězec\03s\032lomítkem\032\092\032a\010novým\035řádkem";
 
-xmlwriter_end_element($xw);
 
-xmlwriter_end_document($xw);
+$testStr = "\010";
+var_dump(quotemeta($testStr));
 
-echo xmlwriter_output_memory($xw);
+
+for ( $pos=0; $pos < strlen($testStr); $pos ++ ) {
+ $byte = substr($testStr, $pos);
+ echo 'Байт ' . $pos . ' строки $str равен ' . ord($byte) . PHP_EOL;
+}
+
+// var_dump(strpos($testStr,'\\'));
+// echo substr($testStr,strpos($testStr,"\\"),strpos($testStr,"\\")+3);
+
+
+// while($str = substr($strT,strpos($strT,"\\"))){
+    
+// }
+// print_r($matches);
+// $comment = strpos($str,"#");
+
+// var_dump($comment);
+
+// echo substr($str,7,-(strlen($str)-$comment));
+
+
+// if(!($comment = strpos($str,"%"))){
+//     $cutLenght = null;
+// }
+// else{
+//     $cutLenght = -(strlen($str)-$comment);
+// }
+
+// $val = substr($str,7,$cutLenght);
+// var_dump(-(strlen($str)-$comment));
+// var_dump($val);
+// $val = substr($str,7,null);
+// var_dump($val);
+// $xw = xmlwriter_open_memory();
+// xmlwriter_set_indent($xw,1);
+// xmlwriter_start_document($xw,'1.0','UTF-8');
+
+// xmlwriter_start_element($xw,"program");
+// xmlwriter_start_attribute($xw,"language");
+// xmlwriter_text($xw,"IPPcode23");
+// xmlwriter_end_attribute($xw);
+
+// xmlwriter_end_element($xw);
+
+// xmlwriter_end_document($xw);
+
+// echo xmlwriter_output_memory($xw);
 
 ?>
--- parse.php
+++ parse.php
@@ -3,29 +3,50 @@ ini_set('display_errors','stderr');
 include 'codeInfo.php';
 
 
+/**
+ * Login via email and password
+ *
+ * @param $numberCode exit code
+ * @param $debugInfo comment for debuging
+ * @param $line which line its happen
+ */
 function exitWithCode($numberCode,$debugInfo,$line){
     //debug message
     //echo $line."\n";
     //echo $debugInfo;
     exit($numberCode);
 }
-
-function itsVar($string){
+/**
+ * @param mixed $checkedString
+ * 
+ * @return bool
+ */
+function itsVar($checkedString){
     
-    if(preg_match("/(LF|GF|TF)@[a-zA-Z_\-&$%*!?][a-zA-Z_\-$&%*!?0-9]*/",$string)){
-        //return mb_substr($string,3);
+    if(preg_match("/(LF|GF|TF)@[a-zA-Z_\-&$%*!?][a-zA-Z_\-$&%*!?0-9]*/",$checkedString)){
         return true;
     }
     return false;
 }
 
-function itsLabel($string){
-    if(preg_match("/^[\$%\*\!_\-\?a-zA-Z]+$/",$string)){
+/**
+ * @param mixed $checkedString
+ * 
+ * @return bool
+ */
+function itsLabel($checkedString){
+    if(preg_match("/^[\$%\*\!_\-\?a-zA-Z]+$/",$checkedString)){
         return true;
     }
     return false;
 }
 
+/**
+ * @param mixed $typeName type of variable
+ * @param mixed $elementNumber count of element
+ * @param mixed $text variable value
+ * 
+ */
 function printXmlElement($typeName,$elementNumber,$text){
     global $xw;
     xmlwriter_start_element($xw,'arg'.($elementNumber+1));
@@ -39,23 +60,34 @@ function printXmlElement($typeName,$elementNumber,$text){
     xmlwriter_end_element($xw);
 }
 
+//work with argv
+if($argc > 1){
+    if($argv[2] == "--help"){
+        if($argc > 2)
+            exit(10);
+        echo "tetno parser prijima IPPcode23 na standartni vstup.\n
+            Ho vystup je XML file\n";
+        echo "Usage: parser.php [options] < inputFile\n";
+        exit(0);
+    }
+}
+//start document
 $xw = xmlwriter_open_memory();
 xmlwriter_set_indent($xw,1);
 xmlwriter_start_document($xw,'1.0','UTF-8');
 
+//number of opcode in queue
 $order = 1;
 $headerIFJ = false;
 
 
 
 while($line = fgets(STDIN)){
-    // var_dump($line);
-    // var_dump(preg_match("/^(\n|#.*)$/",$line));
-    // var_dump(!$headerIFJ);
-    // var_dump(preg_match("/^[ ]*.IPPcode23[ ]*(#.*)?\r?$/",$line));
+    //first we are check on newLine or comment
     if(preg_match("/^(\n|#.*)$/",$line)){
         continue;
     }
+    //then check header
     elseif (!$headerIFJ && preg_match("/^[ ]*.IPPcode23[ ]*(#.*)?\r?$/",$line) )
     {
         xmlwriter_start_element($xw,"program");
@@ -65,14 +97,21 @@ while($line = fgets(STDIN)){
         $headerIFJ = true;
         continue;
     }
+    //continie when have a header
     elseif($headerIFJ)
     {
+        //split the string into an array of individual words
         $splitLine = preg_split("/[\s]+/", trim($line, "\n"));
         
+        //check opcode in language
         if(array_key_exists(strtoupper($splitLine[0]),$language)){
+            //opcode non-register dependent
             $splitLine[0] = strtoupper($splitLine[0]);
 
+            //each operator has a structure
+            //<instruction order="NumberOfOrder" opcode="nameOfOpcode">
             xmlwriter_start_element($xw,'instruction');
+
             xmlwriter_start_attribute($xw,'order');
             xmlwriter_text($xw,$order++);
             xmlwriter_end_attribute($xw);
@@ -80,9 +119,11 @@ while($line = fgets(STDIN)){
             xmlwriter_start_attribute($xw,'opcode');
             xmlwriter_text($xw,$splitLine[0]);
             xmlwriter_end_attribute($xw);
+
+            //check haves opcode arguments or no
             if(isset($language[$splitLine[0]])){
-                //var_dump($splitLine);
-                //var_dump($language[$splitLine[0]]);
+
+                //check code on comment in line
                 $atrCount = count($language[$splitLine[0]]);
                 $extraAtr = 1;
                 for ($i=0; $i < count($splitLine); $i++) { 
@@ -98,8 +139,10 @@ while($line = fgets(STDIN)){
                 }
                 
                 
-               
+                //check each agrument
                 for($i = 0; $i < $atrCount; $i++){
+
+                    //some position have several type of argument
                     $manyOptions = false;
                     if(is_array($language[$splitLine[0]][$i])){
                         $number_of_options = count($language[$splitLine[0]][$i]);
@@ -112,16 +155,15 @@ while($line = fgets(STDIN)){
                     $j = 0;
                     $count_of_errors = 0;
                     while($j < $number_of_options){
-
+                        
+                        //if position have many types op argument its array
+                        //then its variable
                         if($manyOptions){
                             $checkedParam = $language[$splitLine[0]][$i][$j];
                         }
                         else{
                             $checkedParam = $language[$splitLine[0]][$i];
                         }
-                        
-
-                        //echo ($language[$splitLine[0]][$i]->name)."\n";
                         
                         switch($checkedParam){
                             case ParamTypes::variable:
@@ -139,11 +181,20 @@ while($line = fgets(STDIN)){
                                 }
                                 break;
                             case ParamTypes::symbol:
-                                if(preg_match("/^string@*/",$splitLine[$i+1])){
+                                if(preg_match("/^string@.*/",$splitLine[$i+1])){
+                                    
+                                    if(!($comment = strpos($splitLine[$i+1],"#"))){
+                                        $cutLenght = null;
+                                    }
+                                    else{
+                                        $cutLenght = -(strlen($splitLine[$i+1])-$comment);
+                                    }
+                                    $val = substr($splitLine[$i+1],7,$cutLenght);
+                                    
                                     printXmlElement(
                                         "string",
                                         $i,
-                                        substr($splitLine[$i+1],7)
+                                        $val
                                     );
                                 }
                                 elseif(preg_match("/^int@*/",$splitLine[$i+1])){
@@ -236,6 +287,7 @@ while($line = fgets(STDIN)){
             //end element 'instruction'
             xmlwriter_end_element($xw);
         }
+        //maybe its comment
         elseif(preg_match("/#.*/",$line)){
             continue;
         }
@@ -244,14 +296,15 @@ while($line = fgets(STDIN)){
             break;
         }
     }
+    //its not a empty line or comment or header or first having command before a header
     else{
         exitWithCode(21,"21",$line);
     }
 }
-
+//end of document
 xmlwriter_end_element($xw);
-
 xmlwriter_end_document($xw);
+
 if(!$headerIFJ){
     exitWithCode(21,"dont have header","");
 }
